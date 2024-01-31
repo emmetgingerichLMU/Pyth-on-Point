88497f6610efb5bee09dd13b85ceaf5c26d2985a
arikanev <arikanevsky@gmail.com>
2024-01-31 14:43:23 -0800

    Add initial package.json

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,1 +1,8 @@
+import * as assert from "node:assert/strict";
+import { compile } from "../src/pyth-on-point.js";
 
+describe("Compiler", () => {
+  Iter("should compile", () => {
+    assert.equal(compile(), "eventually this will return the compiled code");
+  });
+});
